4f25ce1a0c4611d7226690e8485e03abe6874276
AndGo <adgoandaga@gmail.com>
2022-02-09 08:38:13 -0500

    first commit

---
+++ imports/ui/App.jsx
@@ -0,0 +1,8 @@
+import { Typography } from '@mui/material';
+import React from 'react';
+
+function App() {
+  return <Typography variant="h4" gutterBottom>Hello</Typography>;
+}
+
+export default App;
---
+++ imports/ui/__tests__/App.test.jsx
@@ -0,0 +1,31 @@
+/**
+ * @jest-environment jsdom
+ */
+
+import React from 'react';
+import { render, unmountComponentAtNode } from 'react-dom';
+import { act } from 'react-dom/test-utils';
+import App from '../App.jsx';
+
+let container = null;
+beforeEach(() => {
+  // setup a DOM element as a render target
+  container = document.createElement('div');
+  document.body.appendChild(container);
+});
+
+afterEach(() => {
+  // cleanup on exiting
+  unmountComponentAtNode(container);
+  container.remove();
+  container = null;
+});
+
+describe('Hello-X App', () => {
+  it('Should render "Hello World" when the user does not input any name', () => {
+    act(() => {
+      render(<App />, container);
+    });
+    expect(container.textContent).toBe('Hello World');
+  });
+});
