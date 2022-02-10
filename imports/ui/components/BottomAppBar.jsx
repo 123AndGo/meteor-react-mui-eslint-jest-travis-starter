1f4da4b10d192db7b53a5d840587676377d3bd3c
AndGo <adgoandaga@gmail.com>
2022-02-10 09:47:06 -0500

    Bottom bar is now fully fuctional

--- imports/ui/components/BottomAppBar.jsx
+++ imports/ui/components/BottomAppBar.jsx
@@ -1,12 +1,28 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 
 import { BottomNavigation, BottomNavigationAction, Paper } from '@mui/material';
 
 import EmojiPeopleIcon from '@mui/icons-material/EmojiPeople';
 import HistoryIcon from '@mui/icons-material/History';
+import { useLocation, useNavigate } from 'react-router-dom';
 
 function BottomAppBar() {
-  const [navBarItem, setNavBarItem] = useState(0);
+  const location = useLocation();
+  const activeTab = () => {
+    if (location.pathname === '/history') { return 1; }
+    return 0;
+  };
+
+  const [navBarTab, setNavBarTab] = useState(activeTab());
+  const navigate = useNavigate();
+
+  const handleTabClick = (event, newValue) => {
+    if (newValue === 1) { navigate('/history'); } else { navigate('/'); }
+  };
+
+  const updateActiveTab = () => {
+    useEffect(() => { setNavBarTab(activeTab()); });
+  }; updateActiveTab();
 
   return (
     <Paper
@@ -17,10 +33,8 @@ function BottomAppBar() {
     >
       <BottomNavigation
         showLabels
-        value={navBarItem}
-        onChange={(event, newValue) => {
-          setNavBarItem(newValue);
-        }}
+        value={navBarTab}
+        onChange={handleTabClick}
       >
         <BottomNavigationAction label="Hello" icon={<EmojiPeopleIcon />} />
         <BottomNavigationAction label="History" icon={<HistoryIcon />} />
